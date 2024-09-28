509d1fa1b1b0465fcbb4036d4f06b631f44a5c1e
KARTHICK SAKTHIVEL <54999654+karthick794@users.noreply.github.com>
2024-09-28 12:03:38 +0530

    update employeecontroller

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/controller/EmployeeController.java
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/controller/EmployeeController.java
@@ -14,35 +14,35 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
     public EmployeeServiceImpl employeeService;
 
-    @PostMapping(path = "/createEmployee")
+    @PostMapping()
     public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
         EmployeeDTO savedemployeeDTO = employeeService.createEmployee(employeeDTO);
         return new ResponseEntity<>(savedemployeeDTO, HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "/getEmployee", params = "employeeID")
-    public ResponseEntity<EmployeeDTO> getEmployee(@RequestParam Long employeeID) {
+    @GetMapping(path = "/{employeeID}")
+    public ResponseEntity<EmployeeDTO> getEmployee(@PathVariable  Long employeeID) {
         EmployeeDTO getemployeeDTO = employeeService.getEmployee(employeeID);
         return ResponseEntity.ok(getemployeeDTO);
     }
 
-    @GetMapping(path = "/getAllEmployee")
+    @GetMapping()
     public ResponseEntity<List<EmployeeDTO>> getAllEmployee() {
         List<EmployeeDTO> employeeDTOList = employeeService.getAllEmployee();
         return ResponseEntity.ok(employeeDTOList);
     }
 
-    @PutMapping(path = "/updateemployeedetails",params = "employeeID")
+    @PutMapping(params = "employeeID")
     public ResponseEntity<EmployeeDTO> updateEmployee(@RequestParam Long employeeID,@RequestBody EmployeeDTO employeeDTO) {
        EmployeeDTO employeeDTOList= employeeService.updateEmployeeDetails(employeeID,employeeDTO);
         return ResponseEntity.ok(employeeDTOList);
     }
 
-    @DeleteMapping(path = "/deleteemployeedetails",params = "employeeID")
+    @DeleteMapping(params = "employeeID")
     public ResponseEntity updateEmployee(@RequestParam Long employeeID) {
        employeeService.deleteEmployee(employeeID);
         return ResponseEntity.ok("Employeedetails delete successfully");
